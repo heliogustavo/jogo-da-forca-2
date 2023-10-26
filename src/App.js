39be664269563126452348422111a256d4c87ce0
heliogustavo <jacomehelioiq@gmail.comgit config --global user.name heliogustavogit config --global user.email jacomehelioiq@gmail.com>
2023-10-26 17:41:15 -0400

    Componentes Letras e Jogo criados, dados estáticos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Jogo from "./Jogo";
+import Palavras from "./Palavras"
+import forca0 from "./assets/forca0.png"
+import forca1 from "./assets/forca1.png"
+import forca2 from "./assets/forca2.png"
+import forca3 from "./assets/forca3.png"
+import forca4 from "./assets/forca4.png"
+import forca5 from "./assets/forca5.png"
+import forca6 from "./assets/forca6.png"
+import Letras from "./Letras";
 
-function App() {
+
+
+export default function App() {
+
+  const imgForcas = [forca0, forca1, forca2, forca3, forca4, forca5, forca6]
+  const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Container>
 
-export default App;
+      <Jogo />
+      <Letras />
+
+    </Container>
+  )
+}
+const Container = styled.div`
+width: 100vw;
+height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+background-color: white;
+`
+
+
---
+++ src/Letras.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export default function Letras() {
+
+    return (
+        <Teclado>
+            <Botao>    </Botao>
+        </Teclado>
+    )
+
+}
+
+const Teclado = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 630px;
+    margin-left: 50px;
+    align-items: center;
+`
+const Botao = styled.button`
+    width: 40px;
+    height: 40px;
+    background-color: #e1ecf4;
+    border-radius: 3px;
+    border: 1px solid #7aa7c7;
+    text-transform: uppercase;
+    font-size: 14px;
+    font-weight: 800;
+    color: #39739d;
+    margin: 4px;
+    :hover{
+        background-color: #b3d3ea;
+        color: #2c5777;
+        cursor: pointer;
+    }
+    :disabled{
+        background-color: #9faab5;
+        border: 1px solid #9faab5;
+        color: #79818a;
+        cursor: default;
+    }
+`
